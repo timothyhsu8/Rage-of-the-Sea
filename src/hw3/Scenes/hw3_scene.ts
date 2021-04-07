3ca6dd5f00e05dd980711c41ad567bfae1ae6f21
timothyhsu8 <timothy.hsu@stonybrook.edu>
2021-04-07 09:56:28 -0400

    Changed tilemap to be more fitting

--- src/hw3/Scenes/hw3_scene.ts
+++ src/hw3/Scenes/hw3_scene.ts
@@ -28,7 +28,7 @@ export default class hw3_scene extends Scene {
     private player: AnimatedSprite;
 
     // A list of enemies
-    private enemies: Array<AnimatedSprite>;
+    //private enemies: Array<AnimatedSprite>;
 
     // The wall layer of the tilemap to use for bullet visualization
     private walls: OrthogonalTilemap;
@@ -107,27 +107,25 @@ export default class hw3_scene extends Scene {
         this.initializePlayer();
 
         // Make the viewport follow the player
-        this.viewport.follow(this.player);
-
-        // Zoom in to a reasonable level
-        this.viewport.enableZoom();
-        this.viewport.setZoomLevel(4);
+        //this.viewport.follow(this.player);
+        //this.viewport.enableZoom();
+        this.viewport.setZoomLevel(6);
 
         // Create the navmesh
         this.createNavmesh();
 
         // Initialize all enemies
-        this.initializeEnemies();
+        //this.initializeEnemies();
 
         // Send the player and enemies to the battle manager
-        this.battleManager.setPlayer(<BattlerAI>this.player._ai);
-        this.battleManager.setEnemies(this.enemies.map(enemy => <BattlerAI>enemy._ai));
+        //this.battleManager.setPlayer(<BattlerAI>this.player._ai);
+        //this.battleManager.setEnemies(this.enemies.map(enemy => <BattlerAI>enemy._ai));
 
         // Subscribe to relevant events
-        this.receiver.subscribe("healthpack");
+        //this.receiver.subscribe("healthpack");
 
         // Spawn items into the world
-        this.spawnItems();
+        //this.spawnItems();
 
         // Add a UI for health
         this.addUILayer("health");
@@ -253,7 +251,7 @@ export default class hw3_scene extends Scene {
 
         // Create the player
         this.player = this.add.animatedSprite("player", "primary");
-        this.player.position.set(2*16, 62*16);
+        this.player.position.set(4*16, 4*16);
         this.player.addPhysics(new AABB(Vec2.ZERO, new Vec2(5, 5)));
         this.player.addAI(PlayerController,
             {
@@ -320,43 +318,43 @@ export default class hw3_scene extends Scene {
      * Patrolling enemies are given patrol routes corresponding to the navmesh. The numbers in their route correspond
      * to indices in the navmesh.
      */
-    initializeEnemies(){
-        // Get the enemy data
-        const enemyData = this.load.getObject("enemyData");
+    // initializeEnemies(){
+    //     // Get the enemy data
+    //     const enemyData = this.load.getObject("enemyData");
 
-        // Create an enemies array
-        this.enemies = new Array(enemyData.numEnemies);
+    //     // Create an enemies array
+    //     this.enemies = new Array(enemyData.numEnemies);
 
-        // Initialize the enemies
-        for(let i = 0; i < enemyData.numEnemies; i++){
-            let data = enemyData.enemies[i];
+    //     // Initialize the enemies
+    //     for(let i = 0; i < enemyData.numEnemies; i++){
+    //         let data = enemyData.enemies[i];
 
-            // Create an enemy
-            this.enemies[i] = this.add.animatedSprite("enemy", "primary");
-            this.enemies[i].position.set(data.position[0], data.position[1]);
-            this.enemies[i].animation.play("IDLE");
+    //         // Create an enemy
+    //         this.enemies[i] = this.add.animatedSprite("enemy", "primary");
+    //         this.enemies[i].position.set(data.position[0], data.position[1]);
+    //         this.enemies[i].animation.play("IDLE");
 
-            // Activate physics
-            this.enemies[i].addPhysics(new AABB(Vec2.ZERO, new Vec2(5, 5)));
+    //         // Activate physics
+    //         this.enemies[i].addPhysics(new AABB(Vec2.ZERO, new Vec2(5, 5)));
 
-            if(data.route){
-                data.route = data.route.map((index: number) => this.graph.getNodePosition(index));                
-            }
+    //         if(data.route){
+    //             data.route = data.route.map((index: number) => this.graph.getNodePosition(index));                
+    //         }
 
-            if(data.guardPosition){
-                data.guardPosition = new Vec2(data.guardPosition[0], data.guardPosition[1]);
-            }
+    //         if(data.guardPosition){
+    //             data.guardPosition = new Vec2(data.guardPosition[0], data.guardPosition[1]);
+    //         }
 
-            let enemyOptions = {
-                defaultMode: data.mode,
-                patrolRoute: data.route,            // This only matters if they're a patroller
-                guardPosition: data.guardPosition,  // This only matters if the're a guard
-                health: data.health,
-                player: this.player,
-                weapon: this.createWeapon("weak_pistol")
-            }
+    //         let enemyOptions = {
+    //             defaultMode: data.mode,
+    //             patrolRoute: data.route,            // This only matters if they're a patroller
+    //             guardPosition: data.guardPosition,  // This only matters if the're a guard
+    //             health: data.health,
+    //             player: this.player,
+    //             weapon: this.createWeapon("weak_pistol")
+    //         }
 
-            this.enemies[i].addAI(EnemyAI, enemyOptions);
-        }
-    }
+    //         this.enemies[i].addAI(EnemyAI, enemyOptions);
+    //     }
+    // }
 }
