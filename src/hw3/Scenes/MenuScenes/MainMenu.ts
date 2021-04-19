3dba12d826881920a8cc542fb0f5af1664d62aa7
Edward Huang <ehuang2k@gmail.com>
2021-04-18 20:30:50 -0700

    implemented movement speed and double-edged sword items; todo -> add corresponding sprites

--- src/hw3/Scenes/MenuScenes/MainMenu.ts
+++ src/hw3/Scenes/MenuScenes/MainMenu.ts
@@ -33,7 +33,7 @@ export default class MainMenu extends Scene {
     startScene(){
         // storing out data here
         MainMenu.char = "Diver";
-        MainMenu.equipped = ["anchor", "lasergun", "lasergun", "lasergun", "lasergun", "lasergun"];
+        MainMenu.equipped = ["double-edged", "boots", "anchor", "lasergun", "lasergun", "lasergun", "lasergun", "lasergun"];
         MainMenu.items = ["healthpack", "healthpack", "healthpack", "knife", "knife", "knife", "knife"];
         MainMenu.image = "portrait";
         this.addLayer("primary", 10);
